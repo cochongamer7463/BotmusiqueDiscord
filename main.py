4dfa958333a9a1233cec4de46b4eb3ea255b2a61
cochongamer <cochongamer7463@orange.fr>
2023-10-15 13:32:49 +0200

    Modification systeme telechargement

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ from pytube import YouTube, Playlist, Search, exceptions
 db = sqlite3.connect("music_queue.db")
 SQL = db.cursor()
 
-#create a decorator to time a function
-async def timeit(method):
-    async def timed(*args, **kw):
+
+# create a decorator to time a function
+def timeit(method):
+    def timed(*args, **kw):
         ts = time.time()
-        result = await method(*args, **kw)
+        result = method(*args, **kw)
         te = time.time()
         print('%r  %2.2f sec' % \
               (method.__name__, te - ts))
         return result
-    return await timed
+
+    return timed
 
 
 class test(Client):
@@ -44,7 +46,8 @@ async def on_ready():
                 'server_name text,'
                 'voice_id integer,'
                 'song_url text,'
-                'played boolean'
+                'played boolean,'
+                'downloaded boolean'
                 ')')
     SQL.execute("DELETE FROM music")
     db.commit()
@@ -72,7 +75,7 @@ async def spam(interaction: Interaction, user: discord.User, message: str):
 
 @client.tree.command(name="spam_message_prive")
 async def spam_mp(interaction: Interaction, user: discord.User, nombres: int, message: str):
-    if interaction.user.id == 775466490932625429 or interaction.user.id == 681884655968190540 :
+    if interaction.user.id == 775466490932625429 or interaction.user.id == 681884655968190540:
         if user.id != client.user.id:
             await interaction.response.send_message(";)", ephemeral=True)
             for i in range(0, nombres):
@@ -102,29 +105,37 @@ async def rejoindre(interaction: Interaction):
     except discord.errors.ClientException as e:
         return discord.utils.get(client.voice_clients, guild=interaction.guild)
 
-async def saveToDatabase(interaction: Interaction, urls: list[str] = None, url = None):
+
+async def saveToDatabase(interaction: Interaction, urls: list[str] = None, url=None):
     if urls is None:
-        urls = [url,]
+        urls = [url, ]
     data = list()
     for url in urls:
         data.append((interaction.guild.id,
                      interaction.guild.name,
                      interaction.user.voice.channel.id,
                      url,
+                     False,
                      False))
 
     def save():
         db2 = sqlite3.connect("music_queue.db")
         SQL2 = db2.cursor()
-        SQL2.executemany("INSERT INTO music(server_id, server_name, voice_id,song_url, played) VALUES(?,?,?,?,?)",data)
+        SQL2.executemany(
+            "INSERT INTO music(server_id, server_name, voice_id,song_url, played, downloaded) VALUES(?,?,?,?,?,?)",
+            data)
         db2.commit()
         db2.close()
 
-    threading.Thread(target=save).start()
+    threadSave = threading.Thread(target=save)
+    threadSave.start()
+    while threadSave.is_alive():
+        await asyncio.sleep(1)
 
 
-@client.tree.command(description="commence à jouer les pistes audio de la queue et si indiquer ajoute 1 piste audio à la suite")
-async def play(interaction: Interaction, aleatoire:bool = False, terme: str = None):
+@client.tree.command(
+    description="commence à jouer les pistes audio de la queue et si indiquer ajoute 1 piste audio à la suite")
+async def play(interaction: Interaction, aleatoire: bool = False, terme: str = None):
     await interaction.response.defer()
     try:
         channel_id = interaction.user.voice.channel.id
@@ -136,47 +147,138 @@ async def play(interaction: Interaction, aleatoire:bool = False, terme: str = No
         server_name = interaction.guild.name
         if terme is not None:
             await saveToDatabase(interaction, url=terme)
+        bufferBefore = io.BytesIO()
+        bufferAfter = io.BytesIO()
+        urlBefore = [str(), ]
+        urlAfter = [str(), ]
+        rowidBefore = [int(), ]
+        rowidAfter = [int(), ]
 
-        async def check_queue():
-            db_thread = sqlite3.connect("music_queue.db")
-            SQL_thread = db_thread.cursor()
-            if aleatoire:
-                url, rowid = random.choice(SQL_thread.execute(
-                    'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND played = ? ORdER BY _Rowid_ ASC',
-                    (server_id, server_name, channel_id, False)).fetchall())
+        # @timeit
+        def download(url: str, buffer: io.BytesIO):
+            buffer.seek(0)
+            buffer.truncate(0)
+            if url == "":
+                return
+            if url.find("https://") != -1:
+                YouTube(url).streams.filter(only_audio=True, file_extension="mp4").first().stream_to_buffer(buffer)
             else:
-                url, rowid= SQL_thread.execute(
-                    'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND played = ? ORDER BY _rowid_ ASC',
-                    (server_id, server_name, channel_id, False)).fetchone()
-            buffer = io.BytesIO()
-            def download():
-                if url.find("https://") != -1:
-                    YouTube(url).streams.filter(only_audio=True, file_extension="mp4").first().stream_to_buffer(buffer)
-                else:
-                    Search(url).results[0].streams.filter(only_audio=True,
-                                                          file_extension="mp4").first().stream_to_buffer(buffer)
+                Search(url).results[0].streams.filter(only_audio=True,
+                                                      file_extension="mp4").first().stream_to_buffer(buffer)
 
-            def after(error):
-                if error:
-                    print(error)
-                asyncio.run_coroutine_threadsafe(check_queue(), client.loop)
+        def after(error):
+            if error:
+                print(error)
+            try:
+                asyncio.run_coroutine_threadsafe(check_queueAfter(urlBefore, rowidBefore, urlAfter, rowidAfter),
+                                                 client.loop)
+            except (TypeError, ValueError) as errorCheckQueue:
+                print(errorCheckQueue)
+                asyncio.run_coroutine_threadsafe(interaction.user.voice.channel.send("Queue de lecture vide"),
+                                                 client.loop)
+
+        async def check_queueStart(url_before, rowid_before, url_after, rowid_after):
+            if await check_num_database() < 1:
+                if aleatoire:
+                    (url_before[0], rowid_before[0]), (url_after[0], rowid_after[0]) = random.choices(SQL.execute(
+                        'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORdER BY _Rowid_ ASC',
+                        (server_id, server_name, channel_id, False)).fetchall(), k=2)
+                else:
+                    (url_before[0], rowid_before[0]), (url_after[0], rowid_after[0]) = SQL.execute(
+                        'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORDER BY _rowid_ ASC',
+                        (server_id, server_name, channel_id, False)).fetchmany(size=2)
+            elif not await check_num_database():
+                await interaction.followup.send("pas de son ajouter", ephemeral=True)
+            else:
+                if aleatoire:
+                    url_before[0], rowid_before[0] = random.choice(SQL.execute(
+                        'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORdER BY _Rowid_ ASC',
+                        (server_id, server_name, channel_id, False)).fetchall())
+                else:
+                    url_before[0], rowid_before[0] = SQL.execute(
+                        'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORDER BY _rowid_ ASC',
+                        (server_id, server_name, channel_id, False)).fetchone()
 
             try:
-                threadDownload = threading.Thread(target=download())
-                threadDownload.start()
-                while threadDownload.is_alive():
+                threadDownloadBefore = threading.Thread(target=download, args=(url_before[0], bufferBefore))
+                threadDownloadBefore.start()
+                threadDownloadAfter = threading.Thread(target=download, args=(url_after[0], bufferAfter))
+                threadDownloadAfter.start()
+                SQL.executemany("UPDATE music SET downloaded = ? WHERE _rowid_ = ?",
+                                ((True, rowid_before[0]), (True, rowid_after[0])))
+                db.commit()
+                while threadDownloadBefore.is_alive():
                     await asyncio.sleep(1)
             except (KeyError, exceptions.AgeRestrictedError) as e:
-                await interaction.user.voice.channel.send(f"{url}: lecture impossible dû à une restriction d'âge")
+                await interaction.user.voice.channel.send(
+                    f"{url_before[0]}: lecture impossible dû à une restriction d'âge")
             if voice_client.is_connected():
-                buffer.seek(0)
-                voice_client.play(source=discord.FFmpegPCMAudio(source=buffer, pipe=True), after=after)
-                SQL_thread.execute("UPDATE music SET played = ? WHERE _rowid_ = ?", (True, rowid))
-                db_thread.commit()
+                bufferBefore.seek(0)
+                voice_client.play(source=discord.FFmpegPCMAudio(source=bufferBefore, pipe=True), after=after)
+                SQL.execute("UPDATE music SET played = ? WHERE _rowid_ = ?", (True, rowid_before[0]))
+                db.commit()
+
+        async def check_queueAfter(url_before, rowid_before, url_after, rowid_after):
+            if bufferAfter.getbuffer().nbytes == 0:
+                if await check_num_database() > 0:
+                    if aleatoire:
+                        url_after[0], rowid_after[0] = random.choice(SQL.execute(
+                            'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORdER BY _Rowid_ ASC',
+                            (server_id, server_name, channel_id, False)).fetchall())
+                    else:
+                        url_after[0], rowid_after[0] = SQL.execute(
+                            'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORDER BY _rowid_ ASC',
+                            (server_id, server_name, channel_id, False)).fetchone()
+                    try:
+                        threadDownload = threading.Thread(target=download, args=(url_after[0], bufferAfter))
+                        threadDownload.start()
+                        SQL.execute("UPDATE music SET downloaded = ? WHERE _rowid_ = ?", (True, rowid_after[0]))
+                        db.commit()
+                    except (KeyError, exceptions.AgeRestrictedError) as e:
+                        await interaction.user.voice.channel.send(
+                            f"{url_after[0]}: lecture impossible dû à une restriction d'âge")
+            bufferBefore.seek(0)
+            bufferBefore.truncate(0)
+            bufferBefore.write(bufferAfter.getvalue())
+            url_before[0] = url_after[0]
+            rowid_before[0] = rowid_after[0]
+            bufferAfter.seek(0)
+            bufferAfter.truncate(0)
+
+            if await check_num_database() > 0:
+                if aleatoire:
+                    url_after[0], rowid_after[0] = random.choice(SQL.execute(
+                    'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORdER BY _Rowid_ ASC',
+                    (server_id, server_name, channel_id, False)).fetchall())
+                else:
+                    url_after[0], rowid_after[0] = SQL.execute(
+                    'SELECT song_url, _rowid_ FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? AND downloaded = ? ORDER BY _rowid_ ASC',
+                    (server_id, server_name, channel_id, False)).fetchone()
+            else:
+                await interaction.user.voice.channel.send("Plus de son dans la queue")
+                voice_client.stop()
+                await voice_client.disconnect()
+            try:
+                threadDownload = threading.Thread(target=download, args=(url_after[0], bufferAfter))
+                threadDownload.start()
+                SQL.execute("UPDATE music SET downloaded = ? WHERE _rowid_ = ?", (True, rowid_after[0]))
+                db.commit()
+            except (KeyError, exceptions.AgeRestrictedError) as e:
+                await interaction.user.voice.channel.send(
+                    f"{url_after[0]}: lecture impossible dû à une restriction d'âge")
+            if voice_client.is_connected():
+                bufferBefore.seek(0)
+                voice_client.play(source=discord.FFmpegPCMAudio(source=bufferBefore, pipe=True), after=after)
+                SQL.execute("UPDATE music SET played = ? WHERE _rowid_ = ?", (True, rowid_before[0]))
+                db.commit()
+
+        async def check_num_database():
+            return SQL.execute("SELECT COUNT(*) FROM music WHERE server_id = ? AND server_name = ? AND voice_id = ? and downloaded = ?",
+                               (server_id, server_name, channel_id, False)).fetchone()[0]
 
         try:
-            await check_queue()
-        except TypeError as e:
+            await check_queueStart(urlBefore, rowidBefore, urlAfter, rowidAfter)
+        except (TypeError, ValueError) as e:
             print(e)
             await interaction.followup.send("pas de son ajouter", ephemeral=True)
         else:
@@ -238,7 +340,7 @@ async def add_playlist_to_queue(interaction: Interaction, lien: str):
         await interaction.followup.send("vous n'êtes pas dans un channel", ephemeral=True)
     else:
         message = await interaction.followup.send("Téléchargement de la playlist en cours ...")
-        await saveToDatabase(interaction, urls= [i for i in Playlist(lien).video_urls])
+        await saveToDatabase(interaction, urls=[i for i in Playlist(lien).video_urls])
         await message.edit(content=f"{lien} ajoutée à la queue")
 
 
@@ -252,7 +354,7 @@ async def ajouter_a_la_suite(interaction: Interaction, terme: str):
     else:
         message = await interaction.followup.send("Téléchargement de la musique en cours ...")
         await saveToDatabase(interaction, url=terme)
-        await message.edit(f"{terme} ajoutée a la suit")
+        await message.edit(content=f"{terme} ajoutée a la suit")
 
 
 @client.tree.command(description="Passe à la prochaine musique dans la queue")
@@ -266,25 +368,27 @@ async def suivant(interaction: Interaction):
         voice_client.stop()
         await interaction.response.send_message("chanson passée")
 
+
 class View(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
         self.page = 0
         self.pages = []
+
     def add_page(self, page: str):
         self.pages.append(page)
+
     @discord.ui.button(label="<")
     async def previous(self, interaction: Interaction, button: discord.ui.button):
         if self.page > 0:
-            await interaction.message.edit(content=self.pages[self.page-1])
+            await interaction.message.edit(content=self.pages[self.page - 1])
             self.page -= 1
         await interaction.response.defer()
-
 
     @discord.ui.button(label=">")
     async def next(self, interaction: Interaction, button: discord.ui.button):
         try:
-            await interaction.message.edit(content=self.pages[self.page+1])
+            await interaction.message.edit(content=self.pages[self.page + 1])
         except IndexError:
             pass
         else:
@@ -300,25 +404,30 @@ async def show_queue(interaction: Interaction, joue: bool = None):
     except:
         await interaction.response.send_message("vous n'êtes pas dans un channel", ephemeral=True)
     else:
-        reponse = ["",]
+        reponse = ["", ]
         view = View()
         if joue is None:
-            for index, (ID, name, etat) in enumerate(SQL.execute("SELECT _rowid_, song_url, played FROM music WHERE voice_id = ?", (interaction.user.voice.channel.id,)).fetchall(), 1):
+            for index, (ID, name, etat) in enumerate(
+                    SQL.execute("SELECT _rowid_, song_url, played FROM music WHERE voice_id = ?",
+                                (interaction.user.voice.channel.id,)).fetchall(), 1):
                 reponse[0] += f"{index}: `id: {ID} {name}`, **{'joué' if etat else 'non joué'}**;\n"
         else:
-            for index, (ID, name) in (SQL.execute("SELECT _rowid_, song_url FROM music WHERE voice_id = ? AND played = ?", (interaction.user.voice.channel.id, joue)).fetchall(), 1):
+            for index, (ID, name) in (
+            SQL.execute("SELECT _rowid_, song_url FROM music WHERE voice_id = ? AND played = ?",
+                        (interaction.user.voice.channel.id, joue)).fetchall(), 1):
                 reponse[0] += f"{index}: `id: {ID} {name}`;\n"
         if len(reponse[0]) // 2000:
             reponse.append(reponse[0][:2000].rsplit("\n", 1)[0])
             for i in range(1, (len(reponse[0]) // 2000) + 2):
-                reponse.append(reponse[0][len(reponse[i]):len(reponse[i])+2000].rsplit("\n", 1)[0])
+                reponse.append(reponse[0][len(reponse[i]):len(reponse[i]) + 2000].rsplit("\n", 1)[0])
                 view.add_page(reponse[i])
-            await interaction.followup.send(content=view.pages[0],view=view)
+            await interaction.followup.send(content=view.pages[0], view=view)
         else:
             try:
                 await interaction.followup.send(reponse[0])
             except:
                 await interaction.followup.send("Il n'y a rien dans la queue")
+
 
 @client.tree.command(description="Rejoue une entrée deja jouée par son id")
 async def rejouer(interaction: Interaction, id_chanson: int):
@@ -327,9 +436,11 @@ async def rejouer(interaction: Interaction, id_chanson: int):
     except:
         await interaction.response.send_message("vous n'êtes pas dans un channel", ephemeral=True)
     else:
-        SQL.execute("UPDATE music SET played = ? WHERE _rowid_ = ?", (False, id_chanson))
+        SQL.execute("UPDATE music SET played = ? and downloaded = ? WHERE _rowid_ = ?", (False, False, id_chanson))
         db.commit()
-        await interaction.response.send_message(f"{SQL.execute('SELECT song_url FROM music WHERE _rowid_ = ?', (id_chanson,)).fetchone()[0]} sera rejoué")
+        await interaction.response.send_message(
+            f"{SQL.execute('SELECT song_url FROM music WHERE _rowid_ = ?', (id_chanson,)).fetchone()[0]} sera rejoué")
+
 
 @client.tree.command(description="Supprime une entrée de la queue par son id")
 async def supprimer(interaction: Interaction, id_chanson: int):
@@ -338,10 +449,11 @@ async def supprimer(interaction: Interaction, id_chanson: int):
     except:
         await interaction.response.send_message("vous n'êtes pas dans un channel", ephemeral=True)
     else:
-        song = SQL.execute("SELECT song_url FROM music WHERE _rowid_ = ?", (id_chanson, )).fetchone()[0]
+        song = SQL.execute("SELECT song_url FROM music WHERE _rowid_ = ?", (id_chanson,)).fetchone()[0]
         SQL.execute("DELETE FROM music WHERE _rowid_ = ?", (id_chanson,))
         db.commit()
         await interaction.response.send_message(f"{song}, id: {id_chanson} à été supprimé de la queue")
+
 
 who_spam = []
 list_playlist = []
